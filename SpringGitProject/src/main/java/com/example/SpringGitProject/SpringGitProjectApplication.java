e0af39b7d3b045d2566763d81640400d60fe30a0
happysharwan <iamsharwan21@gmail.com>
2021-10-16 19:32:19 +0530

    first commit

---
+++ SpringGitProject/src/main/java/com/example/SpringGitProject/SpringGitProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.SpringGitProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitProjectApplication.class, args);
+		System.out.println("app is running.........");
+	}
+
+}
---
+++ SpringGitProject/src/test/java/com/example/SpringGitProject/SpringGitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringGitProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
