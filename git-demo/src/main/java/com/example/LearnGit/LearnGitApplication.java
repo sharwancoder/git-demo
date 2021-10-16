8f7ccd1fae20269863c354267a480dfc5d34d535
happysharwan <iamsharwan21@gmail.com>
2021-10-16 21:10:55 +0530

    my chany today

---
+++ git-demo/src/main/java/com/example/LearnGit/LearnGitApplication.java
@@ -0,0 +1,15 @@
+package com.example.LearnGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnGitApplication.class, args);
+		System.out.println("app is running...............");
+		System.out.println("second change.................");
+	}
+
+}
---
+++ git-demo/src/test/java/com/example/LearnGit/LearnGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LearnGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
